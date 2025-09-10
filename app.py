c0cbada245aed16e9536c63dd8bde71be4240a68
ang09356684 <52414411+ang09356684@users.noreply.github.com>
2025-09-10 22:41:04 +0800

    Fix: Add missing imports in app.py
    - Add logging import and setup
    - Add request import from flask
    - Fix logger and request references in request logging functions

--- app.py
+++ app.py
@@ -3,10 +3,36 @@ Flask Application Entry Point
 Load and register blueprints
 """
 
+import logging
+from flask import request
 from app import create_app
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("app")
 
 # Create Flask application instance for WSGI
 app = create_app()
+
+
+
+# Request Logging
+@app.before_request
+def before_request():
+    """Log request information"""
+    logger.info(f'{request.method} {request.path}, '
+                f'args={request.args.to_dict()}, '
+                f'form={request.form.to_dict()}, '
+                f'data={request.get_json(silent=True)}')
+
+@app.after_request
+def after_request(response):
+    """Log response information"""
+    logger.info(f'Response: {request.method} {request.path}, {response.status_code}, '
+                f'data={response.get_data(as_text=True)[:200]}...' if len(response.get_data(as_text=True)) > 200 
+                else f'data={response.get_data(as_text=True)}')
+    return response
+
 
 if __name__ == '__main__':
     # Development environment startup
